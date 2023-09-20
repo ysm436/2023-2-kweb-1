16c72738f962776fe11e67206e9fb315361228ba
ysm436 <endtlr2004@gmail.com>
2023-09-20 20:21:29 +0900

    modify example.js

--- example.js
+++ example.js
@@ -1,1 +1,12 @@
 console.log("hello world");
+
+const a = 1 + 2;
+const b = 1.2;
+const string = `a: ${a} b: ${b}`;
+
+const [x, y, z] = [1, 2, 3];
+console.log(x, y, z);
+
+const c = { m: 10, n: 20, s: "abc" };
+const { m, n, s } = c;
+console.log(`${m},${n},${s}`);
